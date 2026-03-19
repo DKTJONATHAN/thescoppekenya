4f89b6b4caab3a706bda3d1f83bf22f469397a4f
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2026-03-19 09:38:54 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,16 @@
-import { hydrateRoot, createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { HelmetProvider } from "react-helmet-async";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App';
+import './index.css';
 
-const container = document.getElementById("root");
-
-if (container?.hasChildNodes()) {
-  // If the HTML is pre-rendered by react-snap, we hydrate it
-  hydrateRoot(
-    container,
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <HelmetProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </HelmetProvider>
-  );
-} else {
-  // Fallback for normal SPA behavior during local dev
-  createRoot(container!).render(
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  );
-}
+  </React.StrictMode>
+);
