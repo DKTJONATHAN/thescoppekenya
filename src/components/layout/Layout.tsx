2f588bb16b706dab1ad5e0972e646edca3c779f1
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2026-09-12 18:54:07 +0300

    feat(mobile): wire bottom nav + app shell padding

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { PodcastComingSoon } from "./PodcastComingSoon";
+import { MobileBottomNav } from "./MobileBottomNav";
 import { PushNotificationPrompt } from "@/components/PushNotificationPrompt";
 
 interface LayoutProps {
@@ -9,16 +10,17 @@ interface LayoutProps {
 
 export function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col app-shell">
       <a href="#main-content" className="skip-link">
         Skip to content
       </a>
       <Header />
-      <main id="main-content" className="flex-1" tabIndex={-1}>
+      <main id="main-content" className="flex-1 pb-mobile-nav" tabIndex={-1}>
         {children}
       </main>
       <PodcastComingSoon />
       <Footer />
+      <MobileBottomNav />
       <PushNotificationPrompt />
     </div>
   );
