2891002ad51c2a7d771706660cfd418464a6b4c8
MysteryCodes <39903046+DKTJONATHAN@users.noreply.github.com>
2026-09-12 18:04:24 +0300

    feat: install global error monitoring on boot

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import './index.css';
+import { installGlobalErrorHandlers } from './lib/errors';
+
+installGlobalErrorHandlers();
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
